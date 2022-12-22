bb736734c585ac1954389106412694d65e126716
ikoshele <ivan.koshelev2012@gmail.com>
2022-12-22 17:50:09 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,91 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import axios from 'axios';
-import { reactive } from 'vue';
-import {
-  startRegistration,
-  startAuthentication,
-} from '@simplewebauthn/browser';
-
-const state = reactive({ status: 'Click to register' });
-
-async function registerUser() {
-  const { data: resp } = await axios.get(
-    'http://localhost:3000/generate-registration-options'
-  );
-  let attResp;
-  try {
-    // Pass the options to the authenticator and wait for a response
-    attResp = await startRegistration(resp);
-  } catch (error) {
-    // Some basic error handling
-    if (error.name === 'InvalidStateError') {
-      state.status =
-        'Error: Authenticator was probably already registered by user';
-    } else {
-      state.status = error;
-    }
-
-    throw error;
-  }
-  // POST the response to the endpoint that calls
-  // @simplewebauthn/server -> verifyRegistrationResponse()
-  const { data: verificationJSON } = await axios.post(
-    'http://localhost:3000/verify-registration',
-    attResp,
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
-
-  // Show UI appropriate for the `verified` status
-  if (verificationJSON && verificationJSON.verified) {
-    state.status = 'Success!';
-  } else {
-    state.status = `Oh no, something went wrong! Response: <pre>${JSON.stringify(
-      verificationJSON
-    )}</pre>`;
-  }
-  console.log(attResp);
-  console.log(resp);
-}
-
-async function authUser() {
-  const { data: resp } = await axios.get(
-    'http://localhost:3000/generate-authentication-options'
-  );
-  let asseResp;
-  try {
-    // Pass the options to the authenticator and wait for a response
-    asseResp = await startAuthentication(resp);
-  } catch (error) {
-    // Some basic error handling
-    state.status = error;
-    throw error;
-  }
-  // POST the response to the endpoint that calls
-  // @simplewebauthn/server -> verifyAuthenticationResponse()
-  let verificationJSON;
-  try {
-    const { data } = await axios.post(
-      'http://localhost:3000/verify-authentication',
-      asseResp,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    verificationJSON = data;
-  } catch (error: any) {
-    state.status = error.response.data.error;
-  }
-
-  // Show UI appropriate for the `verified` status
-  if (verificationJSON && verificationJSON.verified) {
-    state.status = 'Success login!';
-  }
-}
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-    <div class="status">{{ state.status }}</div>
-    <br />
-    <button type="button" @click="registerUser()">Register</button>
-    <button type="button" @click="authUser()">AuthUser</button>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
   </header>
 
-  <main>
-    <RouterView />
-  </main>
+  <RouterView />
 </template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
--- src/views/LoginView.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <section>
-    <h2>Login page</h2>
-    <form action="#"></form>
-  </section>
-</template>
